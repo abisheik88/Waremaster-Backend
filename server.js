1796803fa9062667723432904d806ad660454cfc
abisheik88 <abishekvijayarangan@gmail.com>
2023-03-01 11:43:55 +0530

    inventory

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
