444bb6b2ef7a9eb2ea503c1c3ad0c3765c729a20
abisheik88 <abishekvijayarangan@gmail.com>
2023-03-03 11:54:12 +0530

    product

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require('body-parser')
 const cors = require('cors');
 const userRoute = require("./routes/userRoute")
+const productRoute = require("./routes/productRoute")
 const errorHandler = require("./middleWare/errorMiddleware")
 const cookieParser = require("cookie-parser");
 
@@ -18,6 +19,7 @@ app.use(cors());
 
 //Routes Middleware
 app.use("/api/users", userRoute)
+app.use("/api/products", productRoute);
 
 //Routes
 app.get("/", (req, res) => {
