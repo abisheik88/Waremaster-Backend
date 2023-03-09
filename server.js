9ffa471fea29b034ab7f225a6132c09ac1d5f25c
abisheik88 <abishekvijayarangan@gmail.com>
2023-03-09 07:40:29 +0530

    cors policy

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000", "https://sprightly-kelpie-1d217e.netlify.app"],
+    credentials: true
+}));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
